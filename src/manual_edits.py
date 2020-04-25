d4c299f92410df8f27284f578b72fed4b0171e81
Kerrick Staley <kerrick@kerrickstaley.com>
2020-04-24 19:18:45 -0700

    Should be keyed off of trad

--- src/manual_edits.py
+++ src/manual_edits.py
@@ -1,11 +1,11 @@
 class ManualEdit:
-    def __init__(simp, defs=None, example_sentences=None):
+    def __init__(trad, defs=None, example_sentences=None):
         """
         Set defs to update defs, or leave as None to use default defs. Same for example_sentences.
 
         Changing other fields, or adjusting rank, via manual edit is not supported.
         """
-        self.simp = simp
+        self.trad = trad
         self.defs = defs
         self.example_sentences = example_sentences
     
@@ -13,7 +13,7 @@ class ManualEdit:
         """
         Changes vocab_word in-place according to this ManualEdit.
         """
-        if vocab_word.simp != self.simp:
+        if vocab_word.trad != self.trad:
             raise RuntimeError('Passed wrong VocabWord to ManualEdit.apply_to_vocab_word')
         
         if self.defs is not None:
