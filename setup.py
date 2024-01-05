c345b654e41a7115a3dcab1bd99d9d01121927b4
Kerrick Staley <k@kerrickstaley.com>
2024-01-04 23:12:14 -0500

    Give the Python package a long_description

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
+from pathlib import Path
 
 version = {}
 with open('chinesevocablist/version.py') as fp:
   exec(fp.read(), version)
 
+repo_root = Path(__file__).parent
+long_description = (repo_root / 'README.md').read_text()
+
 setup(name='chinesevocablist',
       version=version['__version__'],
       description='Programmatic interface to the Chinese Vocab List',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='http://github.com/kerrickstaley/Chinese-Vocab-List',
       author='Kerrick Staley',
       author_email='k@kerrickstaley.com',
